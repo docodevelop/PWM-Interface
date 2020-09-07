d9b358d040c30f6872dbabb1e88ceeb51e1acad4
Do Codevelop <do.codevelop@gmail.com>
2020-09-06 23:18:55 -0500

    Primera versión

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace PWM_Interface
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			this.label4 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(94, 17);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Puerto Serial:";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(13, 34);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(306, 24);
+			this.comboBox1.TabIndex = 1;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(13, 170);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(45, 17);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Valor:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(9, 101);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(46, 17);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "label3";
+			// 
+			// trackBar1
+			// 
+			this.trackBar1.Location = new System.Drawing.Point(12, 202);
+			this.trackBar1.Maximum = 255;
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Size = new System.Drawing.Size(306, 56);
+			this.trackBar1.TabIndex = 4;
+			this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 282);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(46, 17);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "label4";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(125, 388);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "Iniciar";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(125, 459);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "Detener";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// chart2
+			// 
+			chartArea2.Name = "ChartArea1";
+			this.chart2.ChartAreas.Add(chartArea2);
+			legend2.Name = "Legend1";
+			this.chart2.Legends.Add(legend2);
+			this.chart2.Location = new System.Drawing.Point(325, 12);
+			this.chart2.Name = "chart2";
+			series2.ChartArea = "ChartArea1";
+			series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+			series2.Legend = "Legend1";
+			series2.Name = "Series1";
+			this.chart2.Series.Add(series2);
+			this.chart2.Size = new System.Drawing.Size(1093, 735);
+			this.chart2.TabIndex = 9;
+			this.chart2.Text = "chart2";
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.GhostWhite;
+			this.ClientSize = new System.Drawing.Size(1430, 759);
+			this.Controls.Add(this.chart2);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.trackBar1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TrackBar trackBar1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+		private System.IO.Ports.SerialPort serialPort1;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PWM_Interface
+{
+	public partial class Form1 : Form
+	{
+		private string DataBuffer = "";
+		List<string> str = new List<string>();
+		int i = 0;
+		double counter = 0;
+		public Form1()
+		{
+			InitializeComponent();
+			chart2.ChartAreas[0].AxisY.Minimum = 1;
+			chart2.ChartAreas[0].AxisY.Interval = 25;
+			chart2.ChartAreas[0].AxisY.Maximum = 260;
+			if (serialPort1.IsOpen)
+			{
+				serialPort1.Close();
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			string[] availablePorts = SerialPort.GetPortNames();
+			//Se agrega un item por default
+			//Add a default item
+			comboBox1.Items.Add("Selecciona un Puerto Serial");
+			foreach (string port in availablePorts)
+			{
+				comboBox1.Items.Add(port);
+			}
+			//Seleccionar el primero como default
+			//Select the first as default
+			comboBox1.SelectedIndex = 0;
+		}
+
+		private void MostrarErrorPuerto()
+		{
+			MessageBox.Show("Debe seleccionar un Puerto Serial", "Mensaje", MessageBoxButtons.OKCancel);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//Boton iniciar
+			if (serialPort1.IsOpen)
+			{
+				i = 0;
+				timer1.Start();
+				chart2.Series[0].Points.Clear();
+			}
+			else
+			{
+				MostrarErrorPuerto();
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			//Boton detener
+			if (serialPort1.IsOpen)
+			{
+				i = 0;
+				timer1.Stop();
+				chart2.Series[0].Points.Clear();
+			}
+			else
+			{
+				MostrarErrorPuerto();
+			}
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			//Validar que no sea la primera opcion
+			//Validate the selected option
+			if (comboBox1.SelectedIndex > 0)
+			{
+				string portName = comboBox1.SelectedItem.ToString();
+				serialPort1.PortName = portName;
+				//Serial.begin(this is the baud rate value)
+				serialPort1.BaudRate = 115200;
+				serialPort1.Open();
+			}
+		}
+
+		private void trackBar1_ValueChanged(object sender, EventArgs e)
+		{
+			if (serialPort1.IsOpen)
+			{
+				label2.Text = trackBar1.Value.ToString();
+				agregarValores();
+				mandarInfo();
+			}
+			else
+			{
+				MostrarErrorPuerto();
+			}
+		}
+
+		private void agregarValores()
+		{
+			if (serialPort1.IsOpen)
+			{
+				str.Add(trackBar1.Value.ToString());
+				DataBuffer += "&2-" + trackBar1.Value;
+				label4.UseMnemonic = false;
+				label4.Text = DataBuffer.ToString() + "&";
+			}
+			else
+			{
+				MostrarErrorPuerto();
+			}
+		}
+		private void mandarInfo()
+		{
+			counter++;
+			if (serialPort1.IsOpen)
+			{
+				if (DataBuffer != "")
+				{
+					DataBuffer += "&";
+					Console.WriteLine(DataBuffer);
+					serialPort1.Write(DataBuffer);
+					DataBuffer = "";
+					label4.Text = "";
+				}
+				else
+				{
+					MessageBox.Show("Agregue valores al PWM", "Mensaje", MessageBoxButtons.OKCancel);
+				}
+			}
+			else
+			{
+				MostrarErrorPuerto();
+			}
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			i++;
+			if (chart2.Series[0].Points.Count == 20)
+			{
+				chart2.Series[0].Points.Clear();
+				chart2.Series[0].Points.AddXY(i, trackBar1.Value);
+				chart2.Series[0].Points.RemoveAt(0);
+				chart2.Update();
+			}
+			chart2.Series[0].Points.AddXY(i, trackBar1.Value);
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (serialPort1.IsOpen)
+			{
+				serialPort1.Close();
+			}
+		}
+	}
+}
